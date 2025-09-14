af5e0238ce94b0108f57036942869cb0238e46cb
blackberrypineapple <blackberrypineapple@protonmail.com>
2025-09-14 04:33:44 +0000

    add patches from ff-android-patches

--- build.sh
+++ build.sh
@@ -15,11 +15,28 @@ sed -i 's/<color name="button_material_dark">.*/<color name="button_material_dar
 sed -i 's/1c1b22/000000/g' iceraven-patched/assets/extensions/readerview/readerview.css
 sed -i 's/eeeeee/e3e3e3/g' iceraven-patched/assets/extensions/readerview/readerview.css
 
-# Smali patching
-sed -i 's/ff1c1b22/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+# Error page background
+sed -i 's/--background-color: #15141a/--background-color: #000000/g' iceraven-patched/assets/low_and_medium_risk_error_style.css
+sed -i 's/background-color: #1c1b22/background-color: #000000/g' iceraven-patched/assets/extensions/readerview/readerview.css
+sed -i 's/mipmap\/ic_launcher_round/drawable\/ic_launcher_foreground/g' iceraven-patched/res/drawable-v23/splash_screen.xml
+sed -i 's/160\.0dip/200\.0dip/g' iceraven-patched/res/drawable-v23/splash_screen.xml
+
+
+color_subs=(
+	's/ff1c1b22/ff000000/g'
+	's/ff2b2a33/ff000000/g'
+	's/ff42414d/ff15141a/g'
+	's/ff52525e/ff25232e/g'
+	's/ff5b5b66/ff2d2b38/g'
+	's/ff2a2a2e/ff000000/g'
+)
+
+for sub in "${color_subs[@]}"; do
+	sed -i ''"$sub"'' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+done
+
+rm -rf iceraven-patched/assets/extensions/ads
+
 
 # Recompile the APK
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk
