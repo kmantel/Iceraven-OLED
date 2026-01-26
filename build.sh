92e3549d33f233e63d432142f05321240fbf28ce
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2026-01-26 09:01:55 +0100

    Fix drawable patching commands in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-
 # Decompile with Apktool (decode resources + classes)
 wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.0.jar -O apktool.jar
 java -jar apktool.jar d iceraven.apk -o iceraven-patched
-
 # Color patching
 rm -rf iceraven-patched/META-INF
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">#ff000000<\/color>/g' iceraven-patched/res/values-night/colors.xml
@@ -13,22 +11,17 @@ sed -i 's/<color name="fx_mobile_action_color_secondary">.*/<color name="fx_mobi
 sed -i 's/<color name="button_material_dark">.*/<color name="button_material_dark">#ff25242b<\/color>/g' iceraven-patched/res/values/colors.xml
 sed -i 's/1c1b22/000000/g' iceraven-patched/assets/extensions/readerview/readerview.css
 sed -i 's/eeeeee/e3e3e3/g' iceraven-patched/assets/extensions/readerview/readerview.css
-
 # Smali patching
 sed -i 's/ff1c1b22/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
 sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
 sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
 sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-
 # drawable patching
-sed -i 's/mipmap\/ic_launcher_round/drawable\/ic_launcher_foreground/g' patched/res/drawable-v23/splash_screen.xml
-sed -i 's/160\.0dip/200\.0dip/g' patched/res/drawable-v23/splash_screen.xml
-
+sed -i 's/mipmap\/ic_launcher_round/drawable\/ic_launcher_foreground/g' iceraven-patched/res/drawable-v23/splash_screen.xml
+sed -i 's/160\.0dip/200\.0dip/g' iceraven-patched/res/drawable-v23/splash_screen.xml
 # Recompile the APK
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk
-
 # Align and sign the APK
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
-
 # Clean up
-rm -rf iceraven-patched iceraven-patched.apk
+rm -rf iceraven-patched iceraven-patched.apk 
