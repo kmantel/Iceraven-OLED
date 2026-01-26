06fea8f3d5e5ce33bd3129afdf6247d28a69a044
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2026-01-26 14:06:26 +0100

    Modify build script for APK recompilation and alignment
    Restored the --use-aapt2 flag for APK recompilation and updated comments for clarity.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+
 set -e
 
 # Decompile with Apktool (decode resources + classes)
 wget -q https://github.com/iBotPeaches/Apktool/releases/download/v2.11.0/apktool_2.11.0.jar -O apktool.jar
-java -jar apktool.jar d iceraven.apk -o iceraven-patched
-
-# Remove META-INF
+java -jar apktool.jar d iceraven.apk -o iceraven-patched  # -s flag removed
 rm -rf iceraven-patched/META-INF
+
 # Color patching
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">#ff000000<\/color>/g' iceraven-patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values-night/colors.xml
@@ -16,11 +16,11 @@ sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali_classes2/mozilla/component
 sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
 sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
 
-# Recompile the APK (removed --use-aapt2 flag)
-java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk
+# Recompile the APK
+java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 
-# Align the APK
-zipalign -v 4 iceraven-patched.apk iceraven-patched-signed.apk
+# Align and sign the APK
+zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
 
 # Clean up
-rm -rf iceraven-patched iceraven-patched.apk apktool.jar
+rm -rf iceraven-patched iceraven-patch ed.apk
